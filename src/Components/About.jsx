74930b7a8369636b966c11f8c1c7c945013df394
yogitaaax22 <yogitamaheshwari22@gmail.com>
2025-02-19 13:15:32 +0000

    3

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/image.png";
+import image from "../images/this-image.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
